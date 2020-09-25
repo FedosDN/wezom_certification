db0eb3c4824d2c9ddfcfc2c1772d7b3bad0d8b10
Fedor Grechaniy <grechaniy.f.wezom@gmail.com>
2020-09-25 19:27:07 +0300

    added monthly scheduled import

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ImportMakesCommand;
+use App\Console\Commands\ImportModelsCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(ImportMakesCommand::class)->monthly();
+        $schedule->command(ImportModelsCommand::class)->monthly();
     }
 
     /**
